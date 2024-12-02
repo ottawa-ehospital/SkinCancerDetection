777708e3979df5bff0f9d88d7204c95c81efc223
Pulkit Arora <parora.cse18@chitkarauniversity.edu.in>
2024-12-02 14:24:34 -0500

    Enable Cors

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 # The primary goal of this work is to build up a Model of Skin Cancer Detection System utilizing Machine Learning Algorithms. After experimenting with many different architectures for the CNN model It is found that adding the BatchNormalization layer after each Dense, and MaxPooling2D layer can help increase the validation accuracy. In future, a mobile application can be made.
 from flask import Flask, request, render_template
 from PIL import Image
+from flask_cors import CORS  # Import CORS
 import numpy as np
 import skin_cancer_detection as SCD
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 
 @app.route("/", methods=["GET", "POST"])
